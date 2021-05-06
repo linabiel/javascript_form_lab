57296106aac477fa4f139b896b43ef8d1edf9f8d
Daniel Callus <danielcallus@gmail.com>
2021-05-06 15:37:18 +0100

    h tags

--- js/app.js
+++ js/app.js
@@ -16,12 +16,17 @@ const handleFormSubmit = function (event) {
   const category = event.target.category.value;
 
   const listTitle = document.createElement('li');
+  const h2 = document.createElement('h2');
   listTitle.textContent = `${title}`;
-  list.appendChild(listTitle)
+  list.appendChild(h2)
+  h2.appendChild(listTitle)
+
 
   const listAuthor = document.createElement('li');
+  const h3 = document.createElement('h3');
   listAuthor.textContent = `${author}`;
-  list.appendChild(listAuthor)
+  list.appendChild(h3)
+  h3.appendChild(listAuthor)
 
   const listCategory = document.createElement('li');
   listCategory.textContent = `${category}`;
