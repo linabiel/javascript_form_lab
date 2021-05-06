6c68b517b4dba9d7cbea51a572e72e69a6b70c8e
Daniel Callus <danielcallus@gmail.com>
2021-05-06 15:22:48 +0100

    some stuff not working

--- js/app.js
+++ js/app.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
-const form = document.querySelector('#new-item-form');
-form.addEventListener('submit', handleFormSubmit);
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
 
-const list = document.querySelector('ul');
 
-const newListItem = document.createElement('li');
-// newListItem.textContent = 
 
 });
 
+const list = document.querySelector('ul');
 const handleFormSubmit = function (event) {
   event.preventDefault();
   const title = event.target.title.value;
   const author = event.target.author.value;
-  console.log(`${title}  ${author}`)
+  const category = event.target.category.value;
+
+  const listTitle = document.createElement('li');
+  listTitle.textContent = `${title}`;
+  list.appendChild(listTitle)
+
+  const listAuthor = document.createElement('li');
+  listAuthor.textContent = `${author}`;
+  list.appendChild(listAuthor)
+
+  const listCategory = document.createElement('li');
+  listCategory.textContent = `${category}`;
+  list.appendChild(listCategory)
+
 }
-
-
-
-
